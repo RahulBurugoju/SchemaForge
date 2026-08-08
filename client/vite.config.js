097620394a23959f00d15cf66625a171c7266fe8
RahulBurugoju <rahul9399555@gmail.com>
2026-08-08 12:47:26 +0530

    feat: initialize axios instance with token refresh logic and configure Vite build with manual chunking optimization

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,27 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react') || id.includes('react-dom') || id.includes('react-router-dom')) {
+              return 'react-vendor';
+            }
+            if (id.includes('@reduxjs') || id.includes('react-redux')) {
+              return 'redux-vendor';
+            }
+            if (id.includes('@xyflow')) {
+              return 'xyflow-vendor';
+            }
+            if (id.includes('lucide-react')) {
+              return 'lucide-vendor';
+            }
+          }
+        },
+      },
+    },
+  },
 })
